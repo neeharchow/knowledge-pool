5bbd0daac28a9f6770e3128500de5ff66b560d0e
neeharchow <narenchaudhry@gmail.com>
2022-09-08 17:14:00 -0500

    Adds route support for login

--- server/backend/__init__.py
+++ server/backend/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config["SECRET_KEY"] = "pogchampsecret"
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgres://postgres:{os.environ['POSTGRES_PASSWORD']}@postgres:5432/${os.environ['POSTGRES_DB']}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from backend import routes
--- server/backend/routes.py
+++ server/backend/routes.py
@@ -1,10 +1,19 @@
 from backend.models import User, Observation, Pool
-from backend import app
-
+from backend import app, bcrypt, db
+import requests
 
 @app.route("/")
 def main():
     return "Hello"
+
+
+@app.route('/register', methods=["GET", "PUT"])
+def reigster():
+    username = requests.json['username']
+    email = requests.json['email']
+    password = requests.json['password']
+    
+    hashed_password = bcrypt.generate_password_hash
 
 
 @app.route('/api', methods=["GET"])
