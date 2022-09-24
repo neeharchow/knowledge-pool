074d66f230ca59d410be539f370e77660f94e3d5
Naren Chaudhry <90415997+neeharchow@users.noreply.github.com>
2022-09-24 15:58:18 -0500

    adds navbar ui, react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,11 @@
-import "./App.css"
+import "./App.css";
 
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-import NavBar from "./layouts/NavBar"
+import NavBar from "./layouts/NavBar";
 
 function App() {
-	// const [newData, setNewData] = useState([])
-
-	// const url = "/api"
-
-	// useEffect(() => {
-	// 	fetch(url)
-	// 		.then((res) => res.json())
-	// 		.then((data) => {
-	// 			setNewData(data)
-	// 		})
-	// }, [])
-
-	// return <div>{newData.message}</div>
-
-	return <NavBar></NavBar>
+    return <NavBar></NavBar>;
 }
 
-export default App
+export default App;
--- client/src/layouts/NavBar.js
+++ client/src/layouts/NavBar.js
@@ -1,30 +1,65 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Ocean from "../components/Ocean";
+import Pool from "../components/Pool";
+import Dashboard from "../components/Dashboard";
 
+import { Routes, Route, Link } from "react-router-dom";
 
 function NavBar() {
-  return (
-    <Bar>
-        <Stack style={{justifyContent: "left", marginLeft: "15px"}} spacing={3} direction="row">
-            <Button sx={{height: 50}} style={{ color: "white"}} variant="text">Dashboard</Button>
-            <Button sx={{height: 50}} style={{ color: "white"}} variant="text">Pools</Button>
-            <Button sx={{height: 50}} style={{ color: "white"}} variant="text">Oceans</Button>
-        </Stack>
-        
-    </Bar>
-  )
+    return (
+        <Bar>
+            <Routes>
+                <Navigation>
+                    <StyledButton>Dashboard</StyledButton>
+                    <StyledButton>Pools</StyledButton>
+                    <StyledButton>Oceans</StyledButton>
+                </Navigation>
+                <Route path="/ocean">
+                    <Ocean />
+                </Route>
+                <Route path="/pool">
+                    <Pool />
+                </Route>
+                <Route path="/">
+                    <Dashboard />
+                </Route>
+            </Routes>
+        </Bar>
+    );
 }
 
-export default NavBar
+export default NavBar;
+
+const Navigation = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 const Bar = styled.div`
-    color: black;
+    display: flex;
+    align-items: center;
+
     margin: 5px;
     height: 50px;
-    border-radius: 20px;
-    background-color: #3C7DDE;
-    justfy-content: center;
-`
+    border-radius: 15px;
+    background-color: #3c7dde;
+`;
+
+const StyledButton = styled.button`
+    margin: 20px;
+    shadow: none;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    variant: text;
+    background-color: transparent;
+    &:hover {
+        background-color: #5988e0;
+        cursor: pointer;
+    }
+    &:focus {
+        background-color: #7699e0;
+    }
+`;
